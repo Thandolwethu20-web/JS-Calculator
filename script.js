f5fc13f996fe7e3c34d15e5852b1f33620321f5a
Thandolwethu20-web <thandolwethuluyanda33@gmail.com>
2025-06-03 22:50:15 +0200

    donce css and javaScript

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Function to handle operator button presses
 var newLine = true;
+var value1;
+var currentOperator;
 
 // Function to handle operator button presses
 function digitBtnPressed(button) {
@@ -16,4 +18,37 @@ function digitBtnPressed(button) {
 function btnACPressed() {
     document.getElementById("inputBox").value = 0;
     newLine = true
+}
+
+function operatorBtnPressed(operator) {
+    currentOperator = operator;
+    value1 = parseInt(document.getElementById("inputBox").value);
+    newLine = true;    
+}
+
+//Event handler for when the equals button is pressed
+function equalBtnPressed(){
+    var value2 = parseInt(document.getElementById("inputBox").value);
+    var result;
+    var finalTotal;
+
+    switch(currentOperator) {
+        case "+":
+            finalTotal = value1 + value2;
+            break;
+        case "-":
+            finalTotal = value1 - value2;
+            break;          
+        case "/":
+            finalTotal = value1 / value2;
+            break;    
+        case "*":
+            finalTotal = value1 * value2;
+            break;  
+
+    }
+     document.getElementById("inputBox").value = finalTotal;
+     value1 = 0;
+        newLine = true;
+
 }
